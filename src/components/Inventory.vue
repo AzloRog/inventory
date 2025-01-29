cb809c654211bab4eaf5141f4dafdcb23100ed69
Daniel <McsColosPlay@gmail.com>
2025-01-29 19:28:15 +0300

    add bottom bar

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -2,6 +2,7 @@
 import InventoryLeftSideBar from './InventoryLeftSideBar.vue';
 import placeholderImage from "../images/SidebarPlaceholder.jpg"
 import InventoryGrid from './InventoryGrid.vue';
+import InventoryBottomBar from './InventoryBottomBar.vue';
 
 </script>
 
@@ -10,22 +11,29 @@ import InventoryGrid from './InventoryGrid.vue';
         <InventoryLeftSideBar class="inventory-left-sidebar" :imageSource="placeholderImage" title="Sidebar"
             bodyText="lorem8" />
         <InventoryGrid class="inventory-grid" />
+        <InventoryBottomBar class="inventory-bottom" :data="null" :isLoading="true" />
     </div>
 </template>
 
 <style scoped lang="scss">
 .inventory {
     width: 1000px;
-    height: 620px;
+    height: 820px;
     padding: 20px;
     border: 2px solid var(--inventory-border);
 
     display: grid;
     gap: 24px;
     grid-template-columns: 35% auto;
+    grid-template-rows: auto 13%;
 }
 
 .inventory-left-sidebar {}
 
 .inventory-grid {}
+
+.inventory-bottom {
+    grid-column-start: 1;
+    grid-column-end: 3;
+}
 </style>
---
+++ src/components/InventoryBottomBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import CustomSkeleton from './CustomSkeleton.vue';
+
+defineProps<{
+    data: object | null,
+    isLoading: boolean
+}>()
+</script>
+
+<template>
+    <div class="bottomBar">
+        <div v-if="!isLoading">
+            {{ data }}
+        </div>
+        <CustomSkeleton v-else height="100%" width="100%" style="border-radius: 8px;" />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.bottomBar {
+    border: 2px solid var(--inventory-border);
+    background-color: var(--inventory);
+
+    padding: 18px 24px;
+    overflow-y: auto;
+    scrollbar-width: thin;
+    scrollbar-color: var(--inventory-border) var(--surface);
+    line-height: 1.4em;
+
+}
+</style>
